cab73f21ade5b0924235af76e16ad8b43190f88b
Gavin Suddrey <g.suddrey@qut.edu.au>
2021-01-28 14:58:39 +1000

    Install build tools

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const exec = require('@actions/exec');
 
 async function run() {
     const distro = core.getInput('ros-distro');
+    const dependencies = ['python-bloom', 'python-stdeb', 'dh-make']
+
+    await exec.exec(
+		'sudo',
+		['apt', 'install', '-y'] + dependencies
     await exec.exec(
 	 	'sudo',
 	 	['bash', '-c', `source /opt/ros/${distro}/setup.bash && ${__dirname}/release`]
