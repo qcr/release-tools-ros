5c40595b4e3a1830841d94a09afef383ee6f6370
suddrey-qut <38273521+suddrey-qut@users.noreply.github.com>
2021-01-29 14:20:58 +1000

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function run() {
     await exec.exec(
 		'sudo',
 		['apt', 'install', '-y'] + dependencies
+	);
     await exec.exec(
 	 	'sudo',
 	 	['bash', '-c', `source /opt/ros/${distro}/setup.bash && ${__dirname}/release`]
