b857e72f5fb25f853ee490e7ca4faadaa0a993f4
suddrey-qut <38273521+suddrey-qut@users.noreply.github.com>
2021-02-03 10:14:08 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const fs = require('fs');
+
+const fsPromise = fs.promise;
 
 async function run() {
     const distro = core.getInput('ros-distro');
     const dependencies = ['python3-bloom', 'python3-stdeb', 'dh-make']
     
-    let files = '';
-	
-    const options = {};
-    options.listeners = {
-        stdout: (data: Buffer) => {
-            files += data.toString();
-        },
-    };
     await exec.exec(
         'sudo',
         'apt', 'install', '-y'].concat(dependencies)
     );
     await exec.exec(
         'sudo',
-        ['bash', '-c', `source /opt/ros/${distro}/setup.bash && ${__dirname}/release`], 
-        options
-   );
+        ['bash', '-c', `source /opt/ros/${distro}/setup.bash && ${__dirname}/release`]
+    );
+	
+    const filenames = await fsPromise.readdir('./target').filter(f => f.endsWith('.deb'))
 	
     core.setOutput('files', filenames);
+    console.log(filenames)
 }
 
 run();
