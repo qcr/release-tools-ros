eaeb918ca22ae26bd4a860695a5b32bb3c3a6d91
suddrey-qut <38273521+suddrey-qut@users.noreply.github.com>
2021-02-03 10:05:03 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const exec = require('@actions/exec');
 async function run() {
     const distro = core.getInput('ros-distro');
     const dependencies = ['python3-bloom', 'python3-stdeb', 'dh-make']
-
+    
+    let files = '';
+	
+    const options = {};
+    options.listeners = {
+        stdout: (data: Buffer) => {
+            files += data.toString();
+        },
+    };
     await exec.exec(
-		'sudo',
-		['apt', 'install', '-y'].concat(dependencies)
-	);
+        'sudo',
+        'apt', 'install', '-y'].concat(dependencies)
+    );
     await exec.exec(
-	 	'sudo',
-	 	['bash', '-c', `source /opt/ros/${distro}/setup.bash && ${__dirname}/release`]
-	);
+        'sudo',
+        ['bash', '-c', `source /opt/ros/${distro}/setup.bash && ${__dirname}/release`], 
+        options
+   );
+	
+    core.setOutput('files', filenames);
 }
 
 run();
